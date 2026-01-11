5c4233a4c0d29d7f0929c5170af3bb9ce8d61f43
Edward Dillenger <larsseverson1@gmail.com>
2026-01-10 19:46:46 -0600

    Continuation of community; Post page initialization

--- src/routes/community/posts_.$id.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/community/posts_/$id')({
-  component: RouteComponent
-})
-
-function RouteComponent () {
-  return <div>Hello "/community/posts_/$id"!</div>
-}
